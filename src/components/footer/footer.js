9fcffd156bd60c44ae07cea43ec075a98eef046c
Aima Ovai <ovaiaima@gmail.com>
2024-11-06 16:29:30 -0600

    Re-styles bottom nav

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,39 +1,14 @@
 "use client";
+
 import React from "react";
-// import { useState } from "react";
 import styles from "./footer.module.css";
 import logo from "../../../public/images/logo_1.png";
-// import { usePathname } from "next/navigation";
-// import Link from "next/link";
 import Image from "next/image";
+import { CopyrightOutlined, Facebook, Instagram, Phone } from "@mui/icons-material";
+// import { FaTiktok } from "react-icons/fa";
 
 export default function Footer() {
-  // const pathName = usePathname();
-  // const isActive = (path) => path === pathName;
-  // const [nav, setNav] = useState(false);
-  // const onClick = () => setNav(!nav);
-  // const links = [
-  //     {
-  //         name: "Home",
-  //         path: "/home"
-  //     },
-  //     {
-  //         name: "About",
-  //         path: "/about"
-  //     },
-  //     {
-  //         name: "Contact Us",
-  //         path: "/contact-us"
-  //     },
-  //     {
-  //         name: "Services",
-  //         path: "/services"
-  //     },
-  //     {
-  //         name: "Portfolio",
-  //         path: "/portfolio"
-  //     }
-  // ]
+ 
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
@@ -48,38 +23,25 @@ export default function Footer() {
 
         <div className={styles["footer-links-container"]}>
           {/* Company Links */}
-        <div className={styles["footer-links"]}>
-          <div
-            className={styles["footer-li"]}
-            style={{
-              fontWeight: "normal",
-              fontSize: "1.2rem",
-              textTransform: "uppercase",
-            }}
-          >
-            Company
+          <div className={styles["footer-links"]}>
+            <div className={styles["footer-li"]}>
+              <a href="#">About Us</a>
+            </div>
+            <div className={styles["footer-li"]}>
+              <a href="/contact-us">Contact</a>
+            </div>
+            <div className={styles["footer-li"]}>
+              <a href="#">Portfolio</a>
+            </div>
           </div>
-          <div className={styles["footer-li"]}><a href="#">About Us</a></div>
-          <div className={styles["footer-li"]}><a href="#">Contact</a></div>
-          <div className={styles["footer-li"]}><a href="#">Portfolio</a></div>
-        </div>
+          <div className={styles["vertical-line"]}></div>
 
-        {/* Legal Links */}
-        <div className={styles["footer-links"]}>
-          <div
-            className={styles["footer-li"]}
-            style={{
-              fontWeight: "normal",
-              fontSize: "1.2rem",
-              textTransform: "uppercase",
-            }}
-          >
-            Legal
+          {/* Legal Links */}
+          <div className={styles["footer-links"]}>
+            <div className={styles["footer-li"]}><Phone /> 346-558-7736</div>
+            <div className={styles["footer-li"]}> Triple Z Home Solutions | <CopyrightOutlined /> 2024 | All Rights Reserved</div>
+            <div className={styles["footer-li"]}><Instagram /> <Facebook /> {/*<FaTiktok />*/ } </div>
           </div>
-          <div className={styles ["footer-li"]}><a href="#">Legal Notice</a></div>
-          <div className={styles ["footer-li"]}><a href="#">Terms</a></div>
-          <div className={styles ["footer-li"]}><a href="#">Privacy</a></div>
-        </div>
         </div>
       </div>
     </nav>
