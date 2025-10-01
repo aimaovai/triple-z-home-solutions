9d46a7770e560811485a816ad0c687cf263c5c5b
aimaovai <74068109+aimaovai@users.noreply.github.com>
2025-09-30 22:27:01 -0700

    Added testimonial component for featured cutomer testimonials (#2)

---
+++ src/components/testimonialcard/testimonialcard.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import styles from "./testimonialcard.module.css";
+import { ArrowBack, ArrowForward } from "@mui/icons-material";
+
+const Testimonials = () => {
+  const [testimonials, setTestimonials] = useState([]);
+  const [error, setError] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchTestimonials = async () => {
+      try {
+        const response = await fetch('testimonials.json');
+        const data = await response.json();
+        setTestimonials(data.testimonials);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchTestimonials();
+  }, []);
+
+  if (error) {
+    return <div>Error loading testimonials: {error.message}</div>;
+  }
+
+  if (!testimonials.length) {
+    return <div>Loading...</div>;
+  }
+
+  const handleNextTestimonial = () => {
+    setCurrentIndex((currentIndex + 1) % testimonials.length);
+  };
+
+  const handlePrevTestimonial = () => {
+    setCurrentIndex((currentIndex - 1 + testimonials.length) % testimonials.length);
+  };
+
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(<span key={i}>&#9733;</span>);
+      } else {
+        stars.push(<span key={i}>&#9734;</span>);
+      }
+    }
+    return stars;
+  };
+
+
+  return (
+    <div className={styles["testimonials-carousel"]}>
+        <div className="arrows">
+            {/* <button onClick={handlePrevTestimonial}>&lt;</button> */}
+            <ArrowBack style={{ height: "100%", margin: "0 20px", color: "black", cursor: "pointer" }} onClick={handlePrevTestimonial}/>
+        </div>
+      <div className="testimonial">
+        <p style={{ whiteSpace: 'pre-wrap', fontSize: '1.5rem' }}>
+          <strong>{testimonials[currentIndex].name}</strong> <i style={{ whiteSpace: 'pre-wrap', fontSize: '0.8rem' }}>({testimonials[currentIndex].date})</i>
+        </p>
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+          <p style={{ whiteSpace: 'pre-wrap', fontSize: '1rem' }}>
+          {renderStars(testimonials[currentIndex].rating)}
+        </p>
+        <p style={{ whiteSpace: 'pre-wrap', fontSize: '0.5rem' }}>{testimonials[currentIndex].verified && ' (Verified)'}</p>
+        </div>
+        <p style={{ whiteSpace: 'pre-wrap', fontSize: '1rem' }}>
+          {testimonials[currentIndex].text}
+        </p>
+        {/* <ArrowForward style={{ width: "5%", height: "5%", color: "black", cursor: "pointer" }} onClick={() => {}}/> */}
+      </div>
+      <div className={"arrows"} style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
+        {/* <button onClick={handleNextTestimonial}>&gt;</button> */}
+        <ArrowForward style={{ height: "100%", margin: "0 20px", color: "black", cursor: "pointer" }} onClick={handleNextTestimonial}/>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
